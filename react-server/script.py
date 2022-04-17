c20c526b191642b4923966603cfd34903a13e9eb
antieatingactivist <antieatingactivist@gmail.com>
2022-04-17 01:20:08 -0700

    script recreates source directory

--- react-server/script.py
+++ react-server/script.py
@@ -1,7 +1,29 @@
-f = open("demofile2.txt", "a")
-f.write("Now the file has more content!")
-f.close()
+#!/usr/bin/python
 
-#open and read the file after the appending:
-f = open("demofile2.txt", "r")
-print(f.read())
+import os
+
+os.mkdir('payload_test')
+
+# os.chdir('build')
+
+# sourceDir = os.listdir("./static/")
+# print(sourceDir)
+print(os.getcwd())
+def createDirs(workingDirectory):
+
+    sourceDir = os.listdir("./build/" + workingDirectory)
+    print(sourceDir)
+    print(workingDirectory)
+    for i in sourceDir:
+        if os.path.isdir("./build/" + workingDirectory + "/" + i):
+            print('payload_test/' + workingDirectory + "/" + i)
+            os.mkdir('payload_test/' + workingDirectory + "/" + i)
+            # os.chdir(i)
+            
+            createDirs(workingDirectory + "/" + i)
+       
+
+
+createDirs(".")
+
+# print(os.listdir())
