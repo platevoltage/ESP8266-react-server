72fc5208d54edaa6745822319b903722525fbeff
antieatingactivist <antieatingactivist@gmail.com>
2022-07-25 14:27:18 -0700

    file upload now working

--- web-interface/app.py
+++ web-interface/app.py
@@ -12,7 +12,7 @@ def upload_file():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
         uploaded_file.save(uploaded_file.filename)
-    return redirect(url_for('index'))
+    return "<p>File Successfully uploaded!</p>"
 
 # @app.route('/upload', methods=['GET', 'POST'])
 # def upload_file():
