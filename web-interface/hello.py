b90801ceea05a4cd85786e437b1a91b7d0e3ead3
antieatingactivist <antieatingactivist@gmail.com>
2022-07-25 14:06:13 -0700

    initial commit for web interface

---
+++ web-interface/hello.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, redirect, url_for, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
+
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         f = request.files['the_file']
+#         f.save('/var/www/uploads/uploaded_file.txt')
+#         return "<p>Hello, World!</p>"
