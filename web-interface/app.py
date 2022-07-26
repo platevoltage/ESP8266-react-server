09ede8d5b64e728cedabf603479f13f9df3c9f23
antieatingactivist <antieatingactivist@gmail.com>
2022-07-25 21:58:35 -0700

    zipped file now uploadable and unzippable

--- web-interface/app.py
+++ web-interface/app.py
@@ -1,3 +1,5 @@
+
+from zipfile import ZipFile
 from flask import Flask, request, redirect, url_for, render_template
 
 
@@ -6,18 +8,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    # return "<p>test</p>"
 
 @app.route('/', methods=['POST'])
 def upload_file():
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
+    file = request.files['file']
+    zipped_file = file.stream._file
+    with ZipFile(zipped_file, 'r') as zip_ref:
+        zip_ref.extractall('upload')
+
+
     return "<p>File Successfully uploaded!</p>"
 
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         f = request.files['the_file']
-#         f.save('/var/www/uploads/uploaded_file.txt')
-#         return "<p>Hello, World!</p>"
