a73e9423909ecb7bd56cd98aa91ca74b4549f4c7
antieatingactivist <antieatingactivist@gmail.com>
2022-07-29 12:46:17 -0700

    added directory tree to download page

--- web-interface/app.py
+++ web-interface/app.py
@@ -3,7 +3,6 @@ from zipfile import ZipFile
 from flask import Flask, request, render_template, send_from_directory
 import os, espBuilder, shutil
 
-var = "test"
 app = Flask(__name__)
 
 @app.route('/')
@@ -28,8 +27,6 @@ def upload_file():
     with ZipFile(zipped_file, 'r') as zip_ref:       
         zip_ref.extractall('upload')
     
-    workingDirectory = os.listdir('./upload')[-1]
-    print(os.listdir('./upload/' + workingDirectory))
     
     espBuilder.createRootDirectory()
     espBuilder.createDirs(".")
@@ -39,11 +36,32 @@ def upload_file():
     espBuilder.createRestPathChunk()
     espBuilder.createIno()
 
-
+    directoryTree = listDirectoryTree('./product/react-server/')
+    
     shutil.make_archive('./product/react-server', 'zip', './product/react-server')
-    return render_template('index.html', uploadSuccess=True)
+    return render_template('index.html', uploadSuccess=True, directoryTree=directoryTree)
 
 @app.route('/download/')
 def download():
 
     return send_from_directory(directory="./product", path="./react-server.zip")
+
+
+def listDirectoryTree(directory):
+    tree = []
+    text = ''
+    
+    for root, dirs, files in os.walk(directory):
+        level = root.replace(directory, '').count(os.sep)
+        indent = '-' * 4 * (level)
+        # indent = ''
+        tree.append('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = '-' * 4 * (level + 1)
+        # subindent = ''
+        for f in files:
+            tree.append('{}{}'.format(subindent, f))
+    for line in tree:
+        print(line)
+        text += line + '\n'
+    print(tree)
+    return tree
